1aacec33c31894801f9b229980147c49d7abb047
YBouquet <y.bouquet29@gmail.com>
2021-01-31 20:30:08 +0100

    debug dataset generator

--- synthetic_generator.py
+++ synthetic_generator.py
@@ -1,6 +1,6 @@
 from utils.mosaic import *
 from utils.lines import *
-from utils.img_processing.py import = morphological_reconstruction
+from utils.img_processing import morphological_reconstruction
 import utils.prologue as prologue
 import numpy as np
 import cv2
@@ -16,12 +16,12 @@ def saturated_stars(unscaled_img):
     """
     Get a mask with all saturated light blob by thresholding and morphological reconstruction
     """
-    sigma = math.sqrt(stats.describe(unscaled_img.flatten()).variance)
+    sigma = np.std(unscaled_img.flatten())
     indices = np.argwhere(unscaled_img > 3*sigma)
     mask = np.zeros(unscaled_img.shape).astype(np.uint8)
     mask[indices[:,0], indices[:,1]] = 1
     mask_1 = np.zeros(unscaled_img.shape).astype(np.uint8)
-    indices = np.argwhere( unscaled_img > stats.describe(unscaled_img.flatten()).mean)
+    indices = np.argwhere( unscaled_img > np.mean(unscaled_img))
     mask_1[indices[:,0], indices[:,1]] = 1
     final_mask = morphological_reconstruction(mask, mask_1, 2)
     return final_mask #, boxes
@@ -29,7 +29,7 @@ def saturated_stars(unscaled_img):
 
 
 def main(args, seed = 123456789):
-    raw_image,_ = get_raw_image(args.i)#"OMEGA.2020-01-29T03_51_46.345_fullfield_binned.fits")
+    raw_image, unscaled_img = get_raw_image(args.i)#"OMEGA.2020-01-29T03_51_46.345_fullfield_binned.fits")
     crops_addresses = get_blocks_addresses(raw_image)
 
     x_train = []
@@ -39,33 +39,36 @@ def main(args, seed = 123456789):
 
     random.seed(seed)
 
-    for i in range(4):
-        for j in range(8):
+    for i in range(1):
+        for j in range(1):
             x_ = list(crops_addresses.keys())[i]
             crop = get_block(raw_image, x_, crops_addresses[x_][j])
+            unscaled_crop = get_block(unscaled_img, x_, crops_addresses[x_][j])
+
             final_mask = saturated_stars(unscaled_crop) # detect saturated light blob in the image
             mask_dil=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
-            dilation=cv2.dilate(final_mask,mask_dil, iterations=1)
-
+            final_mask=cv2.dilate(final_mask,mask_dil, iterations=1)
             h,w = crop.shape
-            for k in range(args.n): # number of samples generated from a single 64x64 patch
-                subh, subw = 64, 64
-                for alpha in range(0,h, subh):
-                    for beta in range(0,w, subw):
-                        if (alpha + subh) <= h and (beta+subw) <= w :
-                            subcrop = crop[alpha:alpha+subh, beta:beta+subw].copy()
-                            star_indices = np.argwhere(dilation[alpha:alpha+subh, beta:beta+subw] == 1)
+            subh, subw = 64, 64
+            for alpha in range(0,h, subh):
+                for beta in range(0,w, subw):
+                    if (alpha + subh) <= h and (beta+subw) <= w :
+                        for k in range(args.n): # number of samples generated from a single 64x64 patch
+                            subcrop = crop[alpha:alpha+subh, beta:beta+subw]
+                            x_mirror = random.randint(0,1)
+                            y_mirror = random.randint(0,1)
+                            if x_mirror == 1:
+                                subcrop = subcrop[::-1]
+                            if y_mirror == 1:
+                                subcrop = subcrop[:,::-1]
+                            refcrop = subcrop.copy()
+                            subdilation = final_mask[alpha:alpha+subh, beta:beta+subw]
+                            star_indices = np.argwhere(subdilation == 1)
                             replacement_value = np.median(subcrop)
                             subcrop[star_indices[:,0], star_indices[:,1]] = replacement_value # remove outlier pixels
                             max_ = np.max(subcrop)
                             min_ = np.min(subcrop)
                             if max_ != min_ : # white patch
-                                x_mirror = random.randint(0,1)
-                                y_mirror = random.randint(0,1)
-                                if x_mirror == 1:
-                                    subcrop = subcrop[::-1]
-                                if y_mirror == 1:
-                                    subcrop = subcrop[:,::-1]
                                 subcrop = (subcrop - min_) / (max_ - min_) # rescaling
                                 y_true = np.zeros(subcrop.shape)
                                 mask = np.full(subcrop.shape, 0.)
@@ -73,11 +76,10 @@ def main(args, seed = 123456789):
                                 hs = 0
                                 if decision < 0.5:
                                     hs = 1
-
                                     # STREAK PARAMETRIZATION
                                     s_length = random.randint(20,40) # chose the length of the streak
                                     s_width = random.randint(1,3) # chose the width of the streak
-                                    theta = random.randint(0,179) * math.pi / 180. # chose the direction of the streak
+                                    theta = random.randint(0,179) * math.pi / 180. # chose the direction of the streak (in radian)
                                     x_where, y_where = random.randint(0, subh-s_length-1), random.randint(0,subw-s_length-1) # chose the position of the streak
 
                                     # DRAWING
@@ -105,7 +107,7 @@ def main(args, seed = 123456789):
                                     del sat_line
                                     gc.collect()
                                 subcrop = subcrop * (max_ - min_) + min_
-                                subcrop[star_indices[:,0], star_indices[:,1]] = ccrop[alpha:alpha+subh, beta:beta+subw][star_indices[:,0], star_indices[:,1]] # put the blobs in the image back
+                                subcrop[star_indices[:,0], star_indices[:,1]] = refcrop[star_indices[:,0], star_indices[:,1]] # put the blobs in the image back
                                 sub_max = np.max(subcrop)
                                 sub_min = np.min(subcrop)
                                 subcrop = (subcrop - sub_min) / (sub_max - sub_min)
@@ -114,20 +116,21 @@ def main(args, seed = 123456789):
                                 has_satellite.append(hs)
                                 x_train.append([subcrop])
                                 y_train.append(y_true)
-                del y_true
-                del mask
-                del ccrop
-                gc.collect()
-            del crop
+            del y_true
+            del mask
+            del refcrop
+            del subcrop
             gc.collect()
-
-
+    del crop
+    del unscaled_crop
+    del final_mask
+    gc.collect()
 
     if not os.path.exists(DATAPATH):
         os.makedirs(DATAPATH)
-    np.save(datapath + args.o + "_samples.npy", np.array(x_train))
-    np.save(datapath + args.o + "_targets.npy", np.array(y_train))
-    np.save(datapath + args.o + "_patch_targets.npy", np.array(has_satellite))
+    np.save(DATAPATH + args.o + "_samples.npy", np.array(x_train))
+    np.save(DATAPATH + args.o + "_targets.npy", np.array(y_train))
+    np.save(DATAPATH + args.o + "_patch_targets.npy", np.array(has_satellite))
 
 
 if __name__ == '__main__':
